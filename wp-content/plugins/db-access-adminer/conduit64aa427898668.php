3fc9af46a4c6bba990a55c1cc30490d290533cac
THUẬN NGUYỄN ĐÌNH <20127347@student.hcmus.edu.vn>
2023-07-09 12:16:43 +0700

    commit 2

--- wp-content/plugins/db-access-adminer/conduit64aa427898668.php
+++ wp-content/plugins/db-access-adminer/conduit64aa427898668.php
@@ -3,8 +3,8 @@
 return array (
   'url' => 'http://localhost/wordpress_main/index.php?rest_route=/lev0/db-access-adminer/conduit/',
   'cookie_name' => 'wordpress_logged_in_b8f6d4b09f4ae77e881d2a70bc7cb00c',
-  'cookie_hash' => 'bb5677ff397f0a7b6f1e4474b3880ee0d243b9d0',
+  'cookie_hash' => '5aeec4500f5ddc5af5984337564d0d8ed68ee664',
   'auto_submit' => true,
-  'nonce' => 'e8de87d0c2',
+  'nonce' => '5def68ecb8',
 );
 
