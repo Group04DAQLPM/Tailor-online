24853bdb57e041d8d5ba4f6b116ef178eff7d661
Yi <tuanduy18012002@gmail.com>
2023-07-26 20:40:58 +0700

    Best Selling Product Feature

--- wp-content/plugins/db-access-adminer/conduit64c08a8e93fd6.php
+++ wp-content/plugins/db-access-adminer/conduit64c08a8e93fd6.php
@@ -3,8 +3,8 @@
 return array (
   'url' => 'http://localhost/Tailor-online/index.php?rest_route=/lev0/db-access-adminer/conduit/',
   'cookie_name' => 'wordpress_logged_in_fcbb8a96c9a0e93fff086fe7e27763a1',
-  'cookie_hash' => 'f28a71fdf7e2ea54734ff69d4b43ead2371488ee',
+  'cookie_hash' => 'f6c1a6680189b0c1bc2b76643661a4c875a48e6d',
   'auto_submit' => true,
-  'nonce' => 'e5932daefa',
+  'nonce' => '2b8903de49',
 );
 
