5b0b2e91568ac518a6cc2c7652f35e48c3d7c0ef
THUẬN NGUYỄN ĐÌNH <20127347@student.hcmus.edu.vn>
2023-08-16 15:33:58 +0700

    aaaaaaa bug

--- wp-content/themes/tailor/sidebar.php
+++
@@ -1,9 +0,0 @@
-<?php 
-/**
- * The left sidebar template file
-**/ ?>
-<div class="col-md-3 col-sm-4 main-sidebar ">
-    <?php if ( is_active_sidebar( 'sidebar-1' ) ) { 
-		dynamic_sidebar( 'sidebar-1' );
-	} ?>
-</div>
--- wp-content/uploads/uag-plugin/assets/5000/uag-js-4683-1688546441.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
-	UAGBForms.init( {"block_id":"38e357ea","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-38e357ea', 4683 );
-});
-window.addEventListener( 'DOMContentLoaded', function() {
-	UAGBInlineNotice.init( {"c_id":null,"cookies":false,"close_cookie_days":1,"noticeDismiss":"","icon":"rectangle-xmark"}, '.uagb-block-1c881426' );
-});
- });
