13d7b095dc83b5247eedf3d690ead37bf236ac44
THUẬN NGUYỄN ĐÌNH <20127347@student.hcmus.edu.vn>
2023-08-17 14:22:13 +0700

    finalllllll

---
+++ wp-content/plugins/db-access-adminer/conduit64ddc8b38d742.php
@@ -0,0 +1,10 @@
+<?php
+
+return array (
+  'url' => 'http://localhost/Tailor-online/wp-json/lev0/db-access-adminer/conduit/',
+  'cookie_name' => 'wordpress_logged_in_fcbb8a96c9a0e93fff086fe7e27763a1',
+  'cookie_hash' => '912f910e4abe37a4d863e824c9a7069e7cd645fa',
+  'auto_submit' => true,
+  'nonce' => 'a7a2b1d605',
+);
+
